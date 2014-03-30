267d4080b0fa4ab4f914b0f5d5c9d4f1321345eb
Juan Pablo Garibotti Arias <jp.garibotti@gmail.com>
2014-03-30 16:28:12 -0300

    - Changed the Lexer fsm to check the current state to see if it is done.

--- src/LEX/Lexer.cpp
+++ src/LEX/Lexer.cpp
@@ -16,24 +16,71 @@ namespace cfg
         struct LexerData;
         typedef void(*StateFunction)( LexerData& );
 
+        // States
+        void Initial( LexerData & io_lexerData );
+        void FirstCharacter( LexerData & io_lexerData );
+        void ReservedCharacter( LexerData & io_lexerData );
+        void NegativeNumber( LexerData & io_lexerData );
+        void Number( LexerData & io_lexerData );
+        void Name( LexerData & io_lexerData );
+        void String( LexerData & io_lexerData );
+        void Comment( LexerData & io_lexerData );
+        void Decimal( LexerData & io_lexerData );
+        void Escaped( LexerData & io_lexerData );
+
         struct LexerData
         {
             std::ifstream m_configFile;
-            int m_row;
-            int m_column;
+            int m_row, m_rowFirst;
+            int m_column, m_columnFirst;
             std::vector<Token> m_tokenSequence;
             char m_character;
-            bool m_runLexer;
             StateFunction m_currentState;
+
+            LexerData( std::string const& i_configFile );
         };
 
-        void FirstCharacter( LexerData & io_lexerData )
-        {
-            if ( std::isblank( io_lexerData.m_character ) )
-            {
-                
-            }
+        LexerData::LexerData( std::string const& i_configFile )
+            : m_configFile ( i_configFile )
+            , m_currentState ( Initial )
+        {}
 
+        bool IsReserved( char const& i_character );
+
+        char const k_reserved[] =
+        {
+            ';',
+            '[',
+            ']',
+            '{',
+            '}',
+            '=',
+            '\"',
+            '\n'
+        };
+
+        enum class Reserved
+        {
+            Comment,
+            ScopeLeftDelimiter,
+            ScopeRightDelimiter,
+            ScopeTopDelimiter,
+            ScopeBottomDelimiter,
+            Assignment,
+            Quote,
+            NewLine
+        };
+
+        bool IsReserved( char const& i_character )
+        {
+            for ( std::size_t index ( 0 ); index < sizeof( k_reserved ); ++index )
+            {
+                if ( i_character == k_reserved[ index ] )
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         void Initial( LexerData & io_lexerData )
@@ -45,26 +92,28 @@ namespace cfg
             }
             else
             {
-                io_lexerData.m_runLexer = false;
+                io_lexerData.m_currentState = NULL;
             }
         }
 
-        // ReservedCharacter
-        // NegativeNumber
-        // Number
-        // Name
-        // String
-        // Comment
-        // Decimal
-        // Escaped
+        void FirstCharacter( LexerData & io_lexerData )
+        {
+            if ( std::isblank( io_lexerData.m_character ) )
+            {
+                io_lexerData.m_currentState = Initial;
+            }
+            else if ( IsReserved( io_lexerData.m_character ) )
+            {
+                io_lexerData.m_currentState = ReservedCharacter;
+            }
 
-
+        }
 
         std::vector<Token> BuildTokenSequence( std::string i_configFile )
         {
-            LexerData data;
+            LexerData data ( i_configFile );
 
-            while ( data.m_runLexer )
+            while ( data.m_currentState )
             {
                 data.m_currentState( data );
             }
