e1e45d304c66db3979ae37d9b6bd8def42910fca
Juan Pablo Garibotti Arias <jp.garibotti@gmail.com>
2015-01-03 13:13:27 -0300

    + Added begin, end, cbegin and cend methods to Config so it can act as a container.

--- src/CFG/Config.cpp
+++ src/CFG/Config.cpp
@@ -15,63 +15,80 @@
 
 namespace cfg
 {
-    Config::Config()
-    {}
+  Config::Config( std::string const& i_configFile )
+    : m_symbolTable { sem::BuildSymbolTable( syn::BuildSyntaxTree( lex::BuildTokenSequence( i_configFile ) ) ) }
+  {}
 
-    Config::Config( std::string const& i_configFile )
-        : m_symbolTable { sem::BuildSymbolTable( syn::BuildSyntaxTree( lex::BuildTokenSequence( i_configFile ) ) ) }
-    {}
+  void Config::LoadConfiguration( std::string const& i_configFile )
+  {
+    m_symbolTable = sem::BuildSymbolTable( syn::BuildSyntaxTree( lex::BuildTokenSequence( i_configFile ) ) );
+  }
 
-    void Config::LoadConfiguration( std::string const& i_configFile )
+  Value const& Config::GetProperty( std::string const& i_property, Value::Type const& i_type ) const
+  {
+    auto const property ( m_symbolTable.find( i_property ) );
+
+    if ( property != m_symbolTable.end() )
     {
-        m_symbolTable = sem::BuildSymbolTable( syn::BuildSyntaxTree( lex::BuildTokenSequence( i_configFile ) ) );
+      if ( property->second.GetType() == i_type )
+      {
+        return property->second;
+      }
+      else
+      {
+        throw ConfigError ( ConfigError::e_TypeMismatch );
+      }
     }
-
-    Value const& Config::GetProperty( std::string const& i_property, Value::Type const& i_type ) const
+    else
     {
-        auto const property ( m_symbolTable.find( i_property ) );
-
-        if ( property != m_symbolTable.end() )
-        {
-            if ( property->second.GetType() == i_type )
-            {
-                return property->second;
-            }
-            else
-            {
-                throw ConfigError ( ConfigError::e_TypeMismatch );
-            }
-        }
-        else
-        {
-            throw ConfigError ( ConfigError::e_MissingProperty );
-        }
+      throw ConfigError ( ConfigError::e_MissingProperty );
     }
+  }
 
-    bool const& Config::GetBoolProperty( std::string const& i_boolProperty ) const
-    {
-        return GetProperty( i_boolProperty, Value::e_Bool ).GetBool();
-    }
+  bool const& Config::GetBoolProperty( std::string const& i_boolProperty ) const
+  {
+    return GetProperty( i_boolProperty, Value::e_Bool ).GetBool();
+  }
 
-    int const& Config::GetIntProperty( std::string const& i_intProperty ) const
-    {
-        return GetProperty( i_intProperty, Value::e_Int ).GetInt();
-    }
+  int const& Config::GetIntProperty( std::string const& i_intProperty ) const
+  {
+    return GetProperty( i_intProperty, Value::e_Int ).GetInt();
+  }
 
-    float const& Config::GetFloatProperty( std::string const& i_floatProperty ) const
-    {
-        return GetProperty( i_floatProperty, Value::e_Float ).GetFloat();
-    }
+  float const& Config::GetFloatProperty( std::string const& i_floatProperty ) const
+  {
+    return GetProperty( i_floatProperty, Value::e_Float ).GetFloat();
+  }
 
-    std::string const& Config::GetStringProperty( std::string const& i_stringProperty ) const
-    {
-        return GetProperty( i_stringProperty, Value::e_String ).GetString();
-    }
+  std::string const& Config::GetStringProperty( std::string const& i_stringProperty ) const
+  {
+    return GetProperty( i_stringProperty, Value::e_String ).GetString();
+  }
 
-    bool Config::PropertyExists( std::string const& i_propertyName, Value::Type const& i_valueType ) const
-    {
-        auto const property ( m_symbolTable.find( i_propertyName ) );
-        return ( property != m_symbolTable.end() && i_valueType == property->second.GetType() );
-    }
+  bool Config::PropertyExists( std::string const& i_propertyName, Value::Type const& i_valueType ) const
+  {
+    auto const property ( m_symbolTable.find( i_propertyName ) );
+    return ( property != m_symbolTable.end() && i_valueType == property->second.GetType() );
+  }
+  
+  std::map< std::string, Value >::iterator Config::begin() noexcept
+  {
+    return std::begin(m_symbolTable);
+  }
+  
+  std::map< std::string, Value >::iterator Config::end() noexcept
+  {
+    return std::end(m_symbolTable);
+  }
+  
+  std::map< std::string, Value >::const_iterator Config::cbegin() const noexcept
+  {
+    return m_symbolTable.cbegin();
+  }
+  
+  std::map< std::string, Value >::const_iterator Config::cend() const noexcept
+  {
+    return m_symbolTable.cend();
+  }
 }
 
--- src/CFG/Config.hpp
+++ src/CFG/Config.hpp
@@ -14,22 +14,26 @@
 
 namespace cfg
 {
-    class Config
-    {
-        std::map< std::string, Value > m_symbolTable;
+  class Config
+  {
+    std::map< std::string, Value > m_symbolTable;
 
-        Value const& GetProperty( std::string const& i_property, Value::Type const& i_type ) const;
+    Value const& GetProperty( std::string const& i_property, Value::Type const& i_type ) const;
 
-        public:
-            Config();
-            Config( std::string const& i_configFile );
-            void LoadConfiguration( std::string const& i_configFile );
-            bool const& GetBoolProperty( std::string const& i_boolProperty ) const;
-            int const& GetIntProperty( std::string const& i_intProperty ) const;
-            float const& GetFloatProperty( std::string const& i_floatProperty ) const;
-            std::string const& GetStringProperty( std::string const& i_stringProperty ) const;
-            bool PropertyExists( std::string const& i_propertyName, Value::Type const& i_valueType ) const;
-    };
+    public:
+      Config() = default;
+      Config( std::string const& i_configFile );
+      void LoadConfiguration( std::string const& i_configFile );
+      bool const& GetBoolProperty( std::string const& i_boolProperty ) const;
+      int const& GetIntProperty( std::string const& i_intProperty ) const;
+      float const& GetFloatProperty( std::string const& i_floatProperty ) const;
+      std::string const& GetStringProperty( std::string const& i_stringProperty ) const;
+      bool PropertyExists( std::string const& i_propertyName, Value::Type const& i_valueType ) const;
+      std::map< std::string, Value >::iterator begin() noexcept;
+      std::map< std::string, Value >::iterator end() noexcept;
+      std::map< std::string, Value >::const_iterator cbegin() const noexcept;
+      std::map< std::string, Value >::const_iterator cend() const noexcept;
+  };
 }
 
 #endif	/* CONFIG_HPP */
